36a1ce2819b4718f7db226273fa7e6408e875040
289371298 <kaiyan3@illinois.edu>
2021-11-22 23:15:07 -0600

    i

---
+++ get_predictor_script.sh
@@ -0,0 +1,4 @@
+python data/example_dynamics_data/reader.py --mode=0 --name="kitchen"
+python data/example_dynamics_data/reader.py --mode=1 --name="kitchen"
+python data/example_dynamics_data/reader.py --mode=0 --name="office"
+python data/example_dynamics_data/reader.py --mode=1 --name="office"
